c1fccb154c1205fedb9882da772415b272b2572f
Nick Bable <nickbable@Nicks-MBP.lan>
2020-07-05 11:15:33 -0700

    just finished adding in states to the header, in the states & events part of the lesson

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,45 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
+import './header.css'
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
+    this.state = {
+      hasScrolled: false
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = (event) => {
+    const scrollTop = window.pageYOffset
+
+    if (scrollTop > 50) {
+      this.setState({hasScrolled: true})
+    } else {
+      this.setState({hasScrolled: false})
+    }
+  }
+
+
+  render() {
+    return (
+      <div className={this.state.hasScrolled ? 'header header-scrolled' : 'header'}>
+        <div className="header-group">
+          <Link to="/"></Link><img src={require('../images/logo-designcode.svg')} width="30"/><Link/>
+          <Link to="/courses">Courses</Link>
+          <Link to="/downloads">Downloads</Link>
+          <Link to="/workshops">Workshops</Link>
+          <Link to="/buy"><button>Buy</button></Link>
+        </div>
+      </div>
+    )
+  }
 }
 
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
